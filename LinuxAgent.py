686e4052adf5f7f31fd8fc12c3561367b661e908
Vikas Naidu <mvknaidu1@gmail.com>
2026-01-17 12:13:58 +0530

    ddd

--- LinuxAgent.py
+++ LinuxAgent.py
@@ -5,8 +5,55 @@ os.environ["GOOGLE_API_KEY"] = ""
 # AI LLM Model Access
 from langchain_google_genai import ChatGoogleGenerativeAI
 
-google_llm = ChatGoogleGenerativeAI( model="gemini-2.5-flash", temperature=0, max_tokens=1000)
+google_llm = ChatGoogleGenerativeAI( model="gemini-2.5-flash", temperature=0)
 
-response = google_llm.invoke("tell me how to check total ram free in linux")
+# response = google_llm.invoke("how to install package on amazon linux")
 
-print(response.content)
+# print(response.content)
+
+from langchain_community.tools import ShellTool
+
+shell_tool = ShellTool()
+
+# print(shell_tool.run({"commands": ["free -h", "date"]}))
+
+from langchain.agents import create_agent
+from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
+
+tools = [shell_tool]
+
+# Agent
+agent = create_agent(
+    model=google_llm,
+    tools=tools
+
+)
+
+userPrompt = input("Enter Your Requirement ")
+
+system_msg = SystemMessage("""
+                            you are linux administrator, and always give final output only
+                           """)
+
+input_message = HumanMessage(content=userPrompt)
+
+messages = [
+    system_msg,
+    input_message
+]
+
+# Invoke (replacement for .run)
+result = agent.invoke({
+"messages":messages
+})
+
+# print(result)
+
+messages = result["messages"]
+
+# Find the last assistant message (the graph may end on a ToolMessage otherwise)
+last_ai_message = next(m for m in reversed(messages) if isinstance(m, AIMessage))
+
+# Access the content
+final_text_content = last_ai_message.content
+print(f"Final AI response content: {final_text_content}")
